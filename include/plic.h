d885bb4dd95e7b6857041ea57411c19af1a84093
Wade <gao_ziyu@foxmail.com>
2022-12-03 13:38:50 +0000

    support plic, but not test

--- include/plic.h
+++ include/plic.h
@@ -14,34 +14,42 @@ enum class Irq {
 };
 
 constexpr uint64_t kSourcePriorityBase = kPlicBase;
-constexpr uint64_t kSourcePriorityEnd  = kSourcePriorityBase + 0xffc;
+constexpr uint64_t kSourcePriorityEnd  = kSourcePriorityBase + 0xfff;
 
 constexpr uint64_t kPendingBase = kPlicBase + 0x1000;
 constexpr uint64_t kPendingEnd  = kPlicBase + 0x107f;
 
 // two context
 constexpr uint64_t kEnableBase = kPlicBase + 0x2000;
-constexpr uint64_t kEnableEnd  = kPlicBase + 0x20fc;
+constexpr uint64_t kEnableEnd  = kPlicBase + 0x20ff;
+
+// two context
+constexpr uint64_t kThresholdAndClaimBase = kPlicBase + 0x200000;
+constexpr uint64_t kThresholdAndClaimEnd  = kPlicBase + 0x201007;
 
 class PLIC {
 private:
     uint64_t m_clock = 0;
     uint32_t m_irq;
-    uint32_t m_threshold      = 0;
+    uint32_t m_threshold[2]   = { 0 };
+    uint32_t m_claim[2]       = { 0 };
     uint32_t m_priority[1024] = { 0 };
-    uint8_t  m_pending[128]   = { 0 };
-    uint8_t  m_enabled[128]   = { 0 };
+    uint32_t m_pending[32]    = { 0 };
+    uint32_t m_enabled[64]    = { 0 };
     bool     m_needs_update_irq;
     bool     m_virtio_ip_cache;
 
-    void    UpdateIrq(uint64_t& mip);
-    void    SetInterruptPending(const uint32_t irq);
-    void    ClearInterruptPending(const uint32_t irq);
-    uint8_t Load(const uint64_t addr);
+    bool IsInterruptEnabled(const uint8_t context, const uint32_t irq) const;
+    void UpdateClaim(const uint32_t irq);
+    void UpdateIrq(uint64_t& mip);
+    void SetInterruptPending(const uint32_t irq);
+    void ClearInterruptPending(const uint32_t irq);
 
 public:
     PLIC();
-    void Tick(bool virtio_ip, bool uart_ip, uint64_t& mip);
+    void     Tick(bool virtio_ip, bool uart_ip, uint64_t& mip);
+    uint64_t Load(const uint64_t addr);
+    void     Store(const uint64_t addr, const uint32_t data);
 };
 
 } // namespace plic
--- src/plic.cc
+++ src/plic.cc
@@ -1,6 +1,7 @@
 #include "include/plic.h"
 #include "include/csr.h"
 
+#include <cassert>
 #include <cstdint>
 
 namespace rv64_emulator {
@@ -27,6 +28,37 @@ void PLIC::Tick(bool virtio_ip, bool uart_ip, uint64_t& mip) {
     }
 }
 
+bool PLIC::IsInterruptEnabled(const uint8_t context, const uint32_t irq) const {
+    const uint32_t index  = irq / 32;
+    const uint32_t offset = irq % 32;
+    return ((m_enabled[context * 32 + index] >> offset) & 1) == 1;
+}
+
+void PLIC::UpdateClaim(const uint32_t irq) {
+    if (IsInterruptEnabled(1, irq) || irq == 0) {
+        m_claim[1] = irq;
+    }
+}
+void PLIC::SetInterruptPending(const uint32_t irq) {
+    const uint32_t index  = irq / 32;
+    const uint32_t offset = irq % 32;
+    m_pending[index] |= (1 << offset);
+
+    // TODO: 使用哪个版本
+    m_needs_update_irq = true;
+    UpdateClaim(irq);
+}
+
+void PLIC::ClearInterruptPending(const uint32_t irq) {
+    const uint32_t index  = irq / 32;
+    const uint32_t offset = irq % 32;
+    m_pending[index] &= ~(1 << offset);
+
+    // TODO: 使用哪个版本
+    m_needs_update_irq = true;
+    UpdateClaim(0);
+}
+
 void PLIC::UpdateIrq(uint64_t& mip) {
     bool virtio_ip = (m_pending[(uint32_t)Irq::kVirtIO / 8] >> ((uint32_t)Irq::kVirtIO % 8)) & 1;
     bool uart_ip   = (m_pending[(uint32_t)Irq::kUART / 8] >> ((uint32_t)Irq::kUART % 8)) & 1;
@@ -41,12 +73,12 @@ void PLIC::UpdateIrq(uint64_t& mip) {
     uint32_t priority = 0;
 
     // TODO: 这里有针对 virtio 和 uart 中断设置优先级吗
-    if (virtio_ip && virtio_enabled && virtio_priority > priority && virtio_priority > m_threshold) {
+    if (virtio_ip && virtio_enabled && virtio_priority > priority && virtio_priority > m_threshold[1]) {
         irq      = (uint32_t)Irq::kVirtIO;
         priority = virtio_priority;
     }
 
-    if (uart_ip && uart_enabled && uart_priority > priority && uart_priority > m_threshold) {
+    if (uart_ip && uart_enabled && uart_priority > priority && uart_priority > m_threshold[1]) {
         irq      = (uint32_t)Irq::kVirtIO;
         priority = virtio_priority;
     }
@@ -57,22 +89,80 @@ void PLIC::UpdateIrq(uint64_t& mip) {
     }
 }
 
-void PLIC::SetInterruptPending(const uint32_t irq) {
-    const uint32_t index  = irq / 32;
-    const uint32_t offset = irq % 32;
-    m_pending[index] |= (1 << offset);
-    m_needs_update_irq = true;
-}
-
-void PLIC::ClearInterruptPending(const uint32_t irq) {
-    const uint32_t index  = irq / 8;
-    const uint32_t offset = irq % 8;
-    m_pending[index] &= ~(1 << offset);
-    m_needs_update_irq = true;
-}
-
 // https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc#memory-map
-uint8_t PLIC::Load(const uint64_t addr) {
+uint64_t PLIC::Load(const uint64_t addr) {
+    if (addr >= kSourcePriorityBase && addr <= kSourcePriorityEnd) {
+        assert((addr - kSourcePriorityBase) % 4 == 0);
+        const uint32_t index = (addr - kSourcePriorityBase) / 4;
+        return m_priority[index];
+    }
+
+    if (addr >= kPendingBase && addr <= kPendingEnd) {
+        assert((addr - kPendingBase) % 4 == 0);
+        const uint32_t index = (addr - kPendingBase) / 4;
+        return m_pending[index];
+    }
+
+    if (addr >= kEnableBase && addr <= kEnableEnd) {
+        assert((addr - kEnableBase) % 4 == 0);
+        const uint32_t index = (addr - kEnableBase) / 4;
+        return m_enabled[index];
+    }
+
+    if (addr >= kThresholdAndClaimBase && addr <= kThresholdAndClaimEnd) {
+        const uint8_t  context = (addr - kThresholdAndClaimBase) / 0x1000;
+        const uint64_t offset  = (addr - kThresholdAndClaimBase) % 0x1000;
+        switch (offset) {
+            case 0:
+                return m_threshold[context];
+            case 4:
+                return m_claim[context];
+            default:
+                break;
+        }
+    }
+
+    assert(false);
+    return 0;
+}
+
+void PLIC::Store(const uint64_t addr, const uint32_t data) {
+    if (addr >= kSourcePriorityBase && addr <= kSourcePriorityEnd) {
+        assert((addr - kSourcePriorityBase) % 4 == 0);
+        const uint32_t index = (addr - kSourcePriorityBase) / 4;
+        m_priority[index]    = data;
+        return;
+    }
+
+    if (addr >= kPendingBase && addr <= kPendingEnd) {
+        assert((addr - kPendingBase) % 4 == 0);
+        const uint32_t index = (addr - kPendingBase) / 4;
+        m_pending[index]     = data;
+    }
+
+    if (addr >= kEnableBase && addr <= kEnableEnd) {
+        assert((addr - kEnableBase) % 4 == 0);
+        const uint32_t index = (addr - kEnableBase) / 4;
+        m_enabled[index]     = data;
+        return;
+    }
+
+    if (addr >= kThresholdAndClaimBase && addr <= kThresholdAndClaimEnd) {
+        const uint8_t  context = (addr - kThresholdAndClaimBase) / 0x1000;
+        const uint64_t offset  = (addr - kThresholdAndClaimBase) % 0x1000;
+        switch (offset) {
+            case 0:
+                m_threshold[context] = data;
+                break;
+            case 4:
+                ClearInterruptPending(data);
+                break;
+            default:
+                break;
+        }
+    }
+
+    assert(false);
 }
 
 } // namespace plic
