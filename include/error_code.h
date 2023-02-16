4a54f512338e4eaa35f8a7df00f22f05aff2c236
Wade <gao_ziyu@foxmail.com>
2023-02-16 19:06:57 +0800

    init

--- include/error_code.h
+++ include/error_code.h
@@ -8,6 +8,7 @@ enum class ErrorCode : int {
     kDecodeErrorCodeBase = -200,
     kCpuErrorCodeBase    = -300,
     kElfErrorCodeBase    = -400,
+    kPlicErrorCodeBase   = -500,
 };
 
 enum class DramErrorCode : int {
@@ -29,8 +30,21 @@ enum class CpuErrorCode : int {
 };
 
 enum class ElfErrorCode : int {
-    kSuccess      = 0,
+    kSuccess     = 0,
     kInvalidFile = static_cast<int>(ErrorCode::kElfErrorCodeBase) - 1,
+};
+
+enum class PlicErrorCode : int {
+    kSuccess                   = 0,
+    kAddrMisAlign              = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 1,
+    kLoadFailure               = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 2,
+    kStoreFailure              = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 3,
+    kContextReadFailure        = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 4,
+    kContextWriteFailure       = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 5,
+    kPriorityReadFailure       = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 6,
+    kPriorityWriteFailure      = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 7,
+    kContextEnableReadFailure  = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 8,
+    kContextEnableWriteFailure = static_cast<int>(ErrorCode::kPlicErrorCodeBase) - 9,
 };
 
 } // namespace rv64_emulator::errorcode
