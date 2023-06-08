ded2b48881d98f5e0524847c07925ae3537ffb4f
Wade <gao_ziyu@foxmail.com>
2023-06-08 16:03:55 +0800

    google c++ style

--- src/cpu/executor.cc
+++ src/cpu/executor.cc
@@ -46,236 +46,231 @@ static inline constexpr int64_t GetImm(const T desc) {
 Executor::Executor(CPU* cpu) : cpu_(cpu) {}
 
 trap::Trap Executor::RegTypeExec(const decode::DecodeResDesc desc) {
-  const decode::RTypeDesc r_desc =
-      *reinterpret_cast<const decode::RTypeDesc*>(&desc.word);
-  const uint64_t u64_rs1_val = cpu_->GetReg(r_desc.rs1);
-  const uint64_t u64_rs2_val = cpu_->GetReg(r_desc.rs2);
-  const int64_t rs1_val = (int64_t)u64_rs1_val;
-  const int64_t rs2_val = (int64_t)u64_rs2_val;
+  const auto kRegDesc = *reinterpret_cast<const decode::RTypeDesc*>(&desc.word);
+  const uint64_t kU64Rs1Val = cpu_->GetReg(kRegDesc.rs1);
+  const uint64_t kU64Rs2Val = cpu_->GetReg(kRegDesc.rs2);
+  const int64_t kRs1Val = (int64_t)kU64Rs1Val;
+  const int64_t kRs2Val = (int64_t)kU64Rs2Val;
 
   int64_t val = 0;
   switch (desc.token) {
     case decode::InstToken::ADD:
-      val = rs1_val + rs2_val;
+      val = kRs1Val + kRs2Val;
       break;
     case decode::InstToken::SUB:
-      val = rs1_val - rs2_val;
+      val = kRs1Val - kRs2Val;
       break;
     case decode::InstToken::SLL:
-      val = rs1_val << rs2_val;
+      val = kRs1Val << kRs2Val;
       break;
     case decode::InstToken::SLT:
-      val = rs1_val < rs2_val ? 1 : 0;
+      val = kRs1Val < kRs2Val ? 1 : 0;
       break;
     case decode::InstToken::SLTU:
-      val = u64_rs1_val < u64_rs2_val ? 1 : 0;
+      val = kU64Rs1Val < kU64Rs2Val ? 1 : 0;
       break;
     case decode::InstToken::XOR:
-      val = rs1_val ^ rs2_val;
+      val = kRs1Val ^ kRs2Val;
       break;
     case decode::InstToken::SRL:
-      val = u64_rs1_val >> u64_rs2_val;
+      val = kU64Rs1Val >> kU64Rs2Val;
       break;
     case decode::InstToken::SRA:
-      val = rs1_val >> rs2_val;
+      val = kRs1Val >> kRs2Val;
       break;
     case decode::InstToken::OR:
-      val = rs1_val | rs2_val;
+      val = kRs1Val | kRs2Val;
       break;
     case decode::InstToken::AND:
-      val = rs1_val & rs2_val;
+      val = kRs1Val & kRs2Val;
       break;
     case decode::InstToken::MUL:
-      val = rs1_val * rs2_val;
+      val = kRs1Val * kRs2Val;
       break;
     case decode::InstToken::MULH: {
-      const bool kNegativeRes = (rs1_val < 0) ^ (rs2_val < 0);
-      const uint64_t abs_rs1_val = std::abs(rs1_val);
-      const uint64_t abs_rs2_val = std::abs(rs1_val);
-      const uint64_t res = MulUnsignedHi(abs_rs1_val, abs_rs2_val);
-      val = kNegativeRes ? (~res + (rs2_val * rs2_val == 0)) : res;
+      const bool kNegativeRes = (kRs1Val < 0) ^ (kRs2Val < 0);
+      const uint64_t kAbsRs1Val = std::abs(kRs1Val);
+      const uint64_t kAbsRs2Val = std::abs(kRs1Val);
+      const uint64_t kRes = MulUnsignedHi(kAbsRs1Val, kAbsRs2Val);
+      val = kNegativeRes ? (~kRes + (kRs2Val * kRs2Val == 0)) : kRes;
     } break;
     case decode::InstToken::MULHSU: {
-      const bool kNegativeRes = rs1_val < 0;
-      const uint64_t abs_rs1_val = std::abs(rs1_val);
-      const uint64_t res = MulUnsignedHi(abs_rs1_val, u64_rs2_val);
-      val = kNegativeRes ? (~res + (rs1_val * u64_rs2_val == 0)) : res;
+      const bool kNegativeRes = kRs1Val < 0;
+      const uint64_t kAbsRs1Val = std::abs(kRs1Val);
+      const uint64_t kRes = MulUnsignedHi(kAbsRs1Val, kU64Rs2Val);
+      val = kNegativeRes ? (~kRes + (kRs1Val * kU64Rs2Val == 0)) : kRes;
     } break;
     case decode::InstToken::MULHU:
-      val = MulUnsignedHi(u64_rs1_val, u64_rs2_val);
+      val = MulUnsignedHi(kU64Rs1Val, kU64Rs2Val);
       break;
     case decode::InstToken::DIV:
-      if (rs2_val == 0) {
+      if (kRs2Val == 0) {
         val = UINT64_MAX;
-      } else if (rs1_val == INT64_MIN && rs2_val == -1) {
+      } else if (kRs1Val == INT64_MIN && kRs2Val == -1) {
         val = INT64_MIN;
       } else {
-        val = rs1_val / rs2_val;
+        val = kRs1Val / kRs2Val;
       }
       break;
     case decode::InstToken::DIVU:
-      val = u64_rs2_val == 0 ? UINT64_MAX : u64_rs1_val / u64_rs2_val;
+      val = kU64Rs2Val == 0 ? UINT64_MAX : kU64Rs1Val / kU64Rs2Val;
       break;
     case decode::InstToken::REM:
-      if (rs2_val == 0) {
-        val = rs1_val;
-      } else if (rs1_val == INT64_MIN && rs2_val == -1) {
+      if (kRs2Val == 0) {
+        val = kRs1Val;
+      } else if (kRs1Val == INT64_MIN && kRs2Val == -1) {
         val = 0;
       } else {
-        val = rs1_val % rs2_val;
+        val = kRs1Val % kRs2Val;
       }
       break;
     case decode::InstToken::REMU:
-      val = u64_rs2_val == 0 ? u64_rs1_val : u64_rs1_val % u64_rs2_val;
+      val = kU64Rs2Val == 0 ? kU64Rs1Val : kU64Rs1Val % kU64Rs2Val;
       break;
     default:
       break;
   }
 
-  cpu_->SetReg(r_desc.rd, val);
+  cpu_->SetReg(kRegDesc.rd, val);
   return trap::kNoneTrap;
 }
 
 trap::Trap Executor::Rv32TypeExec(const decode::DecodeResDesc desc) {
-  const decode::RTypeDesc r_desc =
-      *reinterpret_cast<const decode::RTypeDesc*>(&desc.word);
-  const int32_t rs1_val = (int32_t)cpu_->GetReg(r_desc.rs1);
-  const int32_t rs2_val = (int32_t)cpu_->GetReg(r_desc.rs2);
+  const auto kRegDesc = *reinterpret_cast<const decode::RTypeDesc*>(&desc.word);
+  const int32_t kRs1Val = (int32_t)cpu_->GetReg(kRegDesc.rs1);
+  const int32_t kRs2Val = (int32_t)cpu_->GetReg(kRegDesc.rs2);
   int64_t val = 0;
 
   switch (desc.token) {
     case decode::InstToken::ADDW:
-      val = rs1_val + rs2_val;
+      val = kRs1Val + kRs2Val;
       break;
     case decode::InstToken::SUBW:
-      val = rs1_val - rs2_val;
+      val = kRs1Val - kRs2Val;
       break;
     case decode::InstToken::SLLW:
-      val = rs1_val << rs2_val;
+      val = kRs1Val << kRs2Val;
       break;
     case decode::InstToken::SRLW:
-      val = (int64_t)(int32_t)((uint32_t)rs1_val >> (uint32_t)rs2_val);
+      val = (int64_t)(int32_t)((uint32_t)kRs1Val >> (uint32_t)kRs2Val);
       break;
     case decode::InstToken::SRAW:
-      val = rs1_val >> rs2_val;
+      val = kRs1Val >> kRs2Val;
       break;
     case decode::InstToken::MULW:
-      val = rs1_val * rs2_val;
+      val = kRs1Val * kRs2Val;
       break;
     case decode::InstToken::DIVW:
-      if (rs2_val == 0) {
+      if (kRs2Val == 0) {
         val = UINT64_MAX;
-      } else if (rs1_val == INT32_MIN && rs2_val == -1) {
+      } else if (kRs1Val == INT32_MIN && kRs2Val == -1) {
         val = INT32_MIN;
       } else {
-        val = (int64_t)(rs1_val / rs2_val);
+        val = (int64_t)(kRs1Val / kRs2Val);
       }
       break;
     case decode::InstToken::DIVUW: {
-      const uint32_t a = (uint32_t)rs1_val;
-      const uint32_t b = (uint32_t)rs2_val;
-      if (b == 0) {
+      const uint32_t kArg1 = (uint32_t)kRs1Val;
+      const uint32_t kArg2 = (uint32_t)kRs2Val;
+      if (kArg2 == 0) {
         val = UINT64_MAX;
       } else {
-        val = (int32_t)(a / b);
+        val = (int32_t)(kArg1 / kArg2);
       }
     } break;
     case decode::InstToken::REMW:
-      if (rs2_val == 0) {
-        val = rs1_val;
-      } else if (rs1_val == INT32_MIN && rs2_val == -1) {
+      if (kRs2Val == 0) {
+        val = kRs1Val;
+      } else if (kRs1Val == INT32_MIN && kRs2Val == -1) {
         val = 0;
       } else {
-        val = (int64_t)((int32_t)(rs1_val % rs2_val));
+        val = (int64_t)((int32_t)(kRs1Val % kRs2Val));
       }
       break;
     case decode::InstToken::REMUW: {
-      const uint32_t a = (uint32_t)rs1_val;
-      const uint32_t b = (uint32_t)rs2_val;
-      val = (int32_t)(b == 0 ? a : a % b);
+      const uint32_t kArg1 = (uint32_t)kRs1Val;
+      const uint32_t kArg2 = (uint32_t)kRs2Val;
+      val = (int32_t)(kArg2 == 0 ? kArg1 : kArg1 % kArg2);
     } break;
     default:
       break;
   }
-  cpu_->SetReg(r_desc.rd, val);
+  cpu_->SetReg(kRegDesc.rd, val);
   return trap::kNoneTrap;
 }
 
 trap::Trap Executor::ImmTypeExec(const decode::DecodeResDesc desc) {
-  const decode::ITypeDesc i_desc =
-      *reinterpret_cast<const decode::ITypeDesc*>(&desc.word);
-  const int64_t rs1_val = (int64_t)cpu_->GetReg(i_desc.rs1);
-  const int32_t imm = i_desc.imm;
-  const uint8_t kShamt = decode::GetShamt(i_desc, false);
+  const auto kImmDesc = *reinterpret_cast<const decode::ITypeDesc*>(&desc.word);
+  const int64_t kRs1Val = (int64_t)cpu_->GetReg(kImmDesc.rs1);
+  const int32_t kImm = kImmDesc.imm;
+  const uint8_t kShamt = decode::GetShamt(kImmDesc, false);
   int64_t val = 0;
   switch (desc.token) {
     case decode::InstToken::ADDI:
-      val = rs1_val + imm;
+      val = kRs1Val + kImm;
       break;
     case decode::InstToken::SLTI:
-      val = rs1_val < imm ? 1 : 0;
+      val = kRs1Val < kImm ? 1 : 0;
       break;
     case decode::InstToken::SLTIU:
-      val = (uint64_t)rs1_val < (uint64_t)imm ? 1 : 0;
+      val = (uint64_t)kRs1Val < (uint64_t)kImm ? 1 : 0;
       break;
     case decode::InstToken::XORI:
-      val = rs1_val ^ imm;
+      val = kRs1Val ^ kImm;
       break;
     case decode::InstToken::ORI:
-      val = rs1_val | imm;
+      val = kRs1Val | kImm;
       break;
     case decode::InstToken::ANDI:
-      val = rs1_val & imm;
+      val = kRs1Val & kImm;
       break;
     case decode::InstToken::SLLI:
-      val = rs1_val << (int64_t)kShamt;
+      val = kRs1Val << (int64_t)kShamt;
       break;
     case decode::InstToken::SRLI:
-      val = (uint64_t)rs1_val >> kShamt;
+      val = (uint64_t)kRs1Val >> kShamt;
       break;
     case decode::InstToken::SRAI:
-      val = rs1_val >> kShamt;
+      val = kRs1Val >> kShamt;
       break;
     default:
       break;
   }
 
-  cpu_->SetReg(i_desc.rd, val);
+  cpu_->SetReg(kImmDesc.rd, val);
   return trap::kNoneTrap;
 }
 
 trap::Trap Executor::Imm32TypeExec(const decode::DecodeResDesc desc) {
-  const decode::ITypeDesc i_desc =
-      *reinterpret_cast<const decode::ITypeDesc*>(&desc.word);
-  const int32_t rs1_val = (int32_t)cpu_->GetReg(i_desc.rs1);
-  const int32_t imm = i_desc.imm;
-  const uint8_t kShamt = decode::GetShamt(i_desc, true);
+  const auto kImmDesc = *reinterpret_cast<const decode::ITypeDesc*>(&desc.word);
+  const int32_t kRs1Val = (int32_t)cpu_->GetReg(kImmDesc.rs1);
+  const int32_t kImm = kImmDesc.imm;
+  const uint8_t kShamt = decode::GetShamt(kImmDesc, true);
   int64_t val = 0;
   switch (desc.token) {
     case decode::InstToken::ADDIW:
-      val = rs1_val + imm;
+      val = kRs1Val + kImm;
       break;
     case decode::InstToken::SLLIW:
-      val = rs1_val << kShamt;
+      val = kRs1Val << kShamt;
       break;
     case decode::InstToken::SRLIW:
-      val = (int64_t)(int32_t)((uint32_t)rs1_val >> kShamt);
+      val = (int64_t)(int32_t)((uint32_t)kRs1Val >> kShamt);
       break;
     case decode::InstToken::SRAIW:
-      val = rs1_val >> kShamt;
+      val = kRs1Val >> kShamt;
       break;
     default:
       break;
   }
 
-  cpu_->SetReg(i_desc.rd, val);
+  cpu_->SetReg(kImmDesc.rd, val);
   return trap::kNoneTrap;
 }
 
 trap::Trap Executor::LuiTypeExec(const decode::DecodeResDesc desc) {
-  const decode::UTypeDesc u_desc =
-      *reinterpret_cast<const decode::UTypeDesc*>(&desc.word);
-  cpu_->SetReg(u_desc.rd, GetImm(u_desc));
+  const auto kUDesc = *reinterpret_cast<const decode::UTypeDesc*>(&desc.word);
+  cpu_->SetReg(kUDesc.rd, GetImm(kUDesc));
   return trap::kNoneTrap;
 }
 
