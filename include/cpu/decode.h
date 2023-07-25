d8bc364874cfc31b1693ce5d3fcafe97172b0780
wadezygao <wadezygao@tencent.com>
2023-07-25 15:25:11 +0800

    other amo instructions

--- include/cpu/decode.h
+++ include/cpu/decode.h
@@ -106,6 +106,24 @@ enum class InstToken {
   LR_D,
   SC_W,
   SC_D,
+  AMOSWAP_D,
+  AMOSWAP_W,
+  AMOADD_D,
+  AMOADD_W,
+  AMOXOR_D,
+  AMOXOR_W,
+  AMOAND_D,
+  AMOAND_W,
+  AMOOR_D,
+  AMOOR_W,
+  AMOMIN_D,
+  AMOMIN_W,
+  AMOMAX_D,
+  AMOMAX_W,
+  AMOMINU_D,
+  AMOMINU_W,
+  AMOMAXU_D,
+  AMOMAXU_W,
 };
 
 using DecodeResDesc = struct DecodeResDesc {
@@ -706,6 +724,132 @@ constexpr InstDesc kInstTable[] = {
         .signature = 0x1800302f,
         .name = "SC.D",
         .token = InstToken::SC_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x800302f,
+        .name = "AMOSWAP.D",
+        .token = InstToken::AMOSWAP_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x800202f,
+        .name = "AMOSWAP.W",
+        .token = InstToken::AMOSWAP_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x302f,
+        .name = "AMOADD.D",
+        .token = InstToken::AMOADD_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x202f,
+        .name = "AMOADD.W",
+        .token = InstToken::AMOADD_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x2000302f,
+        .name = "AMOXOR.D",
+        .token = InstToken::AMOXOR_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x2000202f,
+        .name = "AMOXOR.W",
+        .token = InstToken::AMOXOR_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x6000302f,
+        .name = "AMOAND.D",
+        .token = InstToken::AMOAND_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x6000202f,
+        .name = "AMOAND.W",
+        .token = InstToken::AMOAND_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x4000302f,
+        .name = "AMOOR.D",
+        .token = InstToken::AMOOR_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x4000202f,
+        .name = "AMOOR.W",
+        .token = InstToken::AMOOR_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x8000302f,
+        .name = "AMOMIN.D",
+        .token = InstToken::AMOMIN_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0x8000202f,
+        .name = "AMOMIN.W",
+        .token = InstToken::AMOMIN_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xa000302f,
+        .name = "AMOMAX.D",
+        .token = InstToken::AMOMAX_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xa000202f,
+        .name = "AMOMAX.W",
+        .token = InstToken::AMOMAX_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xc000302f,
+        .name = "AMOMINU.D",
+        .token = InstToken::AMOMINU_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xc000202f,
+        .name = "AMOMINU.W",
+        .token = InstToken::AMOMINU_W,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xe000302f,
+        .name = "AMOMAXU.D",
+        .token = InstToken::AMOMAXU_D,
+    },
+
+    {
+        .mask = 0xf800707f,
+        .signature = 0xe000202f,
+        .name = "AMOMAXU.W",
+        .token = InstToken::AMOMAXU_W,
     },
 };
 
